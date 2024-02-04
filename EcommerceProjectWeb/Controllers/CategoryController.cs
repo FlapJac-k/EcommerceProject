8b19a412403d102bd823cc9c84567e4da3592004
eslam <eslam.mohamed@nweave.com>
2024-02-04 17:38:02 +0200

    Add project files.

---
+++ EcommerceProjectWeb/Controllers/CategoryController.cs
@@ -0,0 +1,67 @@
+﻿using EcommerceProjectWeb.Data;
+using EcommerceProjectWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceProjectWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+
+                return NotFound();
+            }
+
+            Category? category = _db.categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ EcommerceProjectWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcommerceProjectWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
